6da69bd45897f667045ab81ba0af3d4c7afedcb5
Deyu Ming <deyu.ming.16@ucl.ac.uk>
2025-08-05 18:51:49 +0100

    Update initi_py.R
    `newaccelerate` BLAS will now be utilized for NumPy and SciPy on Apple Silicon machines whenever possible.

--- R/initi_py.R
+++ R/initi_py.R
@@ -155,7 +155,7 @@ install_dgpsi <- function(env_name, py_ver, conda_path, dgpsi_ver, reinsatll = F
     message("Installing the required Python packages ...")
   }
   if (Sys.info()[["sysname"]] == "Darwin" & Sys.info()[["machine"]] == "arm64"){
-    reticulate::conda_install(envname = env_name, packages = c(dgpsi_ver, '"libblas=*=*_accelerate"') , conda = conda_path, forge = TRUE, additional_install_args = c('--strict-channel-priority'))
+    reticulate::conda_install(envname = env_name, packages = c(dgpsi_ver, '"libblas=*=*accelerate"') , conda = conda_path, forge = TRUE, additional_install_args = c('--strict-channel-priority'))
   } else if ( isTRUE(grepl("Intel",benchmarkme::get_cpu()$model_name)) ){
     reticulate::conda_install(envname = env_name, packages = c(dgpsi_ver, '"libblas=*=*mkl"') , conda = conda_path, forge = TRUE, additional_install_args = c('--strict-channel-priority') )
     reticulate::conda_install(envname = env_name, packages = c('icc_rt') , channel = c("numba"), conda = conda_path)
