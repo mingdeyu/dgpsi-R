7e1ab265b2340313139f116a9e04116f40c458a2
Deyu Ming <deyu.ming.16@ucl.ac.uk>
2025-10-18 00:13:12 +0100

    Update initi_py.R

--- R/initi_py.R
+++ R/initi_py.R
@@ -45,7 +45,7 @@ init_py <- function(py_ver = NULL, dgpsi_ver = NULL, reinstall = FALSE, uninstal
     ##For devel version
     dgpsi_ver <- c('dill>=0.3.2', 'matplotlib-base>=3.2.1', 'numba >=0.51.2',
                    'numpy >=1.18.2', 'pathos >=0.2.9', 'multiprocess >=0.70.13', 'psutil >=5.8.0',
-                   'scikit-learn >=0.22.0', 'scipy >=1.4.1', 'tqdm >=4.50.2', 'tabulate >=0.8.7')
+                   'scikit-learn >=0.22.0', 'scipy >=1.4.1', 'tqdm >=4.50.2', 'tabulate >=0.8.7', 'faiss-cpu >=1.7.4')
     env_name <- 'dgp_si_R_2_6_0_9000'
     ##For release version
     #dgpsi_ver <- 'dgpsi==2.6.0'
@@ -149,7 +149,6 @@ init_py <- function(py_ver = NULL, dgpsi_ver = NULL, reinstall = FALSE, uninstal
 
 install_dgpsi <- function(env_name, py_ver, conda_path, dgpsi_ver, reinsatll = FALSE) {
   if (!reinsatll) message(sprintf("Setting up the Python environment for %s ...\n", dgpsi_ver))
-  if (!reinsatll) reticulate::conda_create(envname = env_name, python_version = py_ver, conda = conda_path, forge = TRUE, additional_create_args = c('--strict-channel-priority'))
   if (reinsatll) {
     message("Re-installing the required Python packages ...")
   } else {
@@ -160,19 +159,19 @@ install_dgpsi <- function(env_name, py_ver, conda_path, dgpsi_ver, reinsatll = F
     version_nums <- as.numeric(strsplit(macos_version, "\\.")[[1]])
     current_version <- version_nums[1] + version_nums[2] / 10
     if (current_version>=13.3){
-      reticulate::conda_install(envname = env_name, packages = c(dgpsi_ver, '"libblas=*=*newaccelerate"') , conda = conda_path, forge = TRUE, additional_install_args = c('--strict-channel-priority'))
-      if (grepl('9000',env_name)) reticulate::conda_install(envname = env_name, packages = c('faiss-cpu') , channel = c("pytorch/label/nightly"), conda = conda_path)
+      if (!reinsatll) reticulate::conda_create(envname = env_name, packages = c(dgpsi_ver, '"libblas=*=*newaccelerate"'),
+                                               python_version = py_ver, conda = conda_path, forge = TRUE, additional_create_args = c('--strict-channel-priority'))
     } else {
-      if (grepl('9000',env_name)) dgpsi_ver <- c(dgpsi_ver, 'faiss-cpu >=1.7.4')
-      reticulate::conda_install(envname = env_name, packages = c(dgpsi_ver, '"libblas=*=*accelerate"') , conda = conda_path, forge = TRUE, additional_install_args = c('--strict-channel-priority'))
+      if (!reinsatll) reticulate::conda_create(envname = env_name, packages = c(dgpsi_ver, '"libblas=*=*accelerate"'),
+                                               python_version = py_ver, conda = conda_path, forge = TRUE, additional_create_args = c('--strict-channel-priority'))
     }
   } else if ( isTRUE(grepl("Intel",benchmarkme::get_cpu()$model_name)) ){
-    if (grepl('9000',env_name)) dgpsi_ver <- c(dgpsi_ver, 'faiss-cpu >=1.7.4')
-    reticulate::conda_install(envname = env_name, packages = c(dgpsi_ver, '"libblas=*=*mkl"') , conda = conda_path, forge = TRUE, additional_install_args = c('--strict-channel-priority') )
-    reticulate::conda_install(envname = env_name, packages = c('icc_rt') , channel = c("numba"), conda = conda_path)
+    if (Sys.info()[["sysname"]] %in% c("Windows", "Linux")) dgpsi_ver <- c(dgpsi_ver, 'intel-cmplr-lib-rt')
+    if (!reinsatll) reticulate::conda_create(envname = env_name, packages = c(dgpsi_ver, '"libblas=*=*mkl"'),
+                                             python_version = py_ver, conda = conda_path, forge = TRUE, additional_create_args = c('--strict-channel-priority'))
   } else {
-    if (grepl('9000',env_name)) dgpsi_ver <- c(dgpsi_ver, 'faiss-cpu >=1.7.4')
-    reticulate::conda_install(envname = env_name, packages = c(dgpsi_ver) , conda = conda_path, forge = TRUE, additional_install_args = c('--strict-channel-priority'))
+    if (!reinsatll) reticulate::conda_create(envname = env_name, packages = c(dgpsi_ver),
+                                             python_version = py_ver, conda = conda_path, forge = TRUE, additional_create_args = c('--strict-channel-priority'))
   }
   if (grepl('9000',env_name)) {
     if (reinsatll) {
